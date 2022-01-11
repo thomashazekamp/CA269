da9b3336a7499567178563e66474433e06afebff
thomashazekamp <thomas.hazekamp@gmail.com>
2022-01-11 16:43:40 +0000

    new files

---
+++ Week01/Hello.java
@@ -0,0 +1,9 @@
+public class Hello
+{
+    /* Printing hello to output*/
+    
+    public static void main(String []args)
+    {
+        System.out.println("Hello There"); // Prints hello there to output
+    }
+}

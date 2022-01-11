2da60529287a67762f7ac23f13f178bbf2a367ea
thomashazekamp <thomas.hazekamp@gmail.com>
2022-01-11 16:51:14 +0000

    new files

---
+++ Week01/Hello3.java
@@ -0,0 +1,12 @@
+public class Hello3
+{
+    /* Printing hello to output*/
+    
+    public static void main(String []args)
+    {
+        for (int i = 0; i < 3; i++) // This loops 3 times
+        {
+            System.out.println("Hello"); // Prints hello to output
+        }
+    }
+}
